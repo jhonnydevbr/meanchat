17120a0689f7de97e7deb79ec4485e08ebd06605
jhonnydevbr <jhonnybrustolin@gmail.com>
2024-04-23 14:03:51 -0300

    Criado controller para cadastro de usuário

---
+++ src/controllers/usuario.controller.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from "express";
+import usuarioModel from "../models/usuario.model";
+
+class UsuarioController {
+
+    public async cadastrar(req: Request, res: Response): Promise<Response>{
+        const usuario = await usuarioModel.create(req.body);
+        return res.json(usuario);
+    }
+
+}
+
+export default new UsuarioController();
