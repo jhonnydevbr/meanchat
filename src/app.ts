10550ef043008a33f0b093e53a88407ab7603027
jhonnydevbr <jhonnybrustolin@gmail.com>
2024-04-23 13:45:39 -0300

    Configuração inicial App.ts e conexão com o MongoDB

---
+++ src/app.ts
@@ -0,0 +1,40 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose, { mongo } from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const MONGODBURI = process.env.MONGODB_URI ?? '';
+
+
+export class App {
+
+    private express: express.Application;
+    private porta = process.env.PORTA;
+    
+
+    constructor() {
+        this.express = express();
+        this.listen();
+        this.database();
+    }
+
+    public getApp(): express.Application {
+        return this.express;
+    }
+
+    private listen(): void {
+        this.express.listen(this.porta, ()=>{
+            console.log('Servidor iniciado na porta: ' + this.porta);
+        });
+    }
+
+    private database(): void {
+        mongoose.connect(MONGODBURI).then(() => {
+            console.log('Conexão com MongoDB estabelecida com sucesso');
+        }).catch(err => {
+            console.error('Erro ao conectar ao MongoDB:', err);
+        });
+    }
+
+}
